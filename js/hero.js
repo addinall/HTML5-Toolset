74e8e4be27a96ea0ab62e0ad595b0531cc7d5c43
addinall <addinall@addinall.net>
2013-04-22 18:47:53 +1000

    After a merge mess

--- js/hero.js
+++ js/hero.js
@@ -3,7 +3,6 @@
 // CAPTAIN  SLOG
 //---------------------------------------------------------
 //
-<<<<<<< HEAD
 //  FILE:       hero.js
 //  SYSTEM:     My new Websiite 
 //  AUTHOR:     Mark Addinall
@@ -12,18 +11,6 @@
 //              I could never figure out why they where 
 //              so big fat and ugly.  So as part of the
 //              toolkit rewrite, a tiny little hero slider.
-=======
-//	FILE:       hero.js
-//	SYSTEM:     My new Websiite 
-//	AUTHOR:     Mark Addinall
-//	DATE:       22/04/2013
-//	SYNOPSIS:   After using sliders for many many years,
-//				I could never figure out why they where 
-//				so big fat and ugly.  So as part of the
-//				toolkit rewrite, a tiny little hero slider.
->>>>>>> 5cb952f78489c2bcda2770a35ec2aa81dd8faa27
-//
-//
 //------------+-------------------------------+------------
 // DATE       |    CHANGE                     |    WHO
 //------------+-------------------------------+------------
@@ -33,7 +20,6 @@
 //
 
 (function($){
-<<<<<<< HEAD
     $.fn.Hero = function(interval) {                    // extend jQuery
 
     var slides;                                         // array of hero slides
@@ -63,37 +49,7 @@
 
     setTimeout(run, interval);
     };
-=======
-	$.fn.Hero = function(interval) {					// extend jQuery
-
-	var slides;											// array of hero slides
-	var number;
-	var index;
-
-	//--------------
-	function run() {
-
-		// hide previous image and showing next
-		$(slides[index]).fadeOut(1000);
-		index++;
-
-		if (index >= number) { 
-			index = 0;
-		}
-
-		$(slides[index]).fadeIn(1000);
-
-		// loop
-		setTimeout(run, interval);
-	}
-
-	slides = $('#hero').children();
-	number = slides.length;
-	index=0;
-
-	setTimeout(run, interval);
-	};
->>>>>>> 5cb952f78489c2bcda2770a35ec2aa81dd8faa27
+	
 })(jQuery);
 
 //
